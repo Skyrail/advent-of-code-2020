011cbd6daabb060540af25c34b4ce347f481b984
Aled Parry <aled.skyrail@gmail.com>
2020-12-01 15:02:22 +0000

    Day 1 - on looking at the other solutions I realised I could tidy this up which calms the soul a little - also means I can run both parts in the same file

--- day1/main.py
+++ day1/main.py
@@ -1,24 +1,30 @@
-import os, sys
+import os, sys  
+from itertools import combinations
+from functools import reduce
 
 if len(sys.argv) != 2:
     raise ValueError('Please supply the intcode to be processed')
 
 inputPath = sys.argv[1]
 
+def findExpensesThatEqual(expenses, result, count):
+    combos = combinations(expenses, count)
+
+    for combo in combos:
+        if sum(combo) == result:
+            return combo
+
 if os.path.isfile(inputPath):
     try:
         file = open(inputPath)
         
         expenses = list(map(int, file.read().split()))
-        values = expenses.copy()
 
-        for expense in expenses:
-            for valueToAdd in expenses.copy():
-                for secondValueToAdd in expenses.copy():
-                    if (expense + valueToAdd + secondValueToAdd) == 2020:
-                        print(f'Expense values are {expense} and {valueToAdd} and {secondValueToAdd} giving {expense * valueToAdd * secondValueToAdd}')
-                        break
+        part1Combination = findExpensesThatEqual(expenses, 2020, 2)
+        part2Combination = findExpensesThatEqual(expenses, 2020, 3)
 
+        print(f'Part 1: {part1Combination} multiplied equals {reduce(lambda x,y: x*y, part1Combination)}')
+        print(f'Part 2: {part2Combination} multiplied equals {reduce(lambda x,y: x*y, part2Combination)}')
 
     except IOError:
         print(f'Unable to open file {inputPath}')
