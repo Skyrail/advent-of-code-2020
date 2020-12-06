a628ddf1d2af8ee21c421cc561e5395f914262e5
Aled Parry <aled.skyrail@gmail.com>
2020-12-06 12:22:14 +0000

    Day 6 - part 1 - success! Yay for Python Sets

---
+++ day6/main.py
@@ -0,0 +1,26 @@
+import os, sys  
+
+if len(sys.argv) != 2:
+    raise ValueError('Please supply the input to be processed')
+
+inputPath = sys.argv[1]
+
+if os.path.isfile(inputPath):
+    try:
+        file = open(inputPath)
+        
+        batch = list(file.read().strip().split('\n\n'))
+
+        total = 0
+
+        for group in batch:
+            total += len(set(group.replace('\n', '')))
+
+        print(f'The total is {total}')
+
+    except IOError:
+        print(f'Unable to open file {inputPath}')
+    finally:
+        file.close()
+else:
+    print(f'Unable to locate file {inputPath}')
