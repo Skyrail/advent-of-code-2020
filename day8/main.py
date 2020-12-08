48f92c7ba1888c498d81c1e57895cacb90c8b16d
Aled Parry <aled.skyrail@gmail.com>
2020-12-08 10:32:56 +0000

    Day 8 - part 2 - success! Albeit a glorified brute force, it was fairly quick and I can't wrap my head around any other way of doing it

--- day8/main.py
+++ day8/main.py
@@ -5,30 +5,52 @@ if len(sys.argv) != 2:
 
 inputPath = sys.argv[1]
 
+def runBatch(batch):
+
+    processedInst = []
+    idx = accumulator = 0
+
+    while idx < len(batch):
+        instruction,value = batch[idx].split(' ')
+
+        if idx in processedInst:
+            return False, accumulator
+        else:
+            processedInst.append(idx)
+
+        if instruction == 'acc':
+            accumulator += int(value)
+            idx += 1
+        elif instruction == 'jmp':
+            idx += int(value)
+        else:
+            idx += 1
+
+    return True, accumulator
+
+
 if os.path.isfile(inputPath):
     try:
         file = open(inputPath)
         
         batch = list(file.read().strip().split('\n'))
-        processedInst = []
-        idx = accumulator = 0
 
-        while idx < len(batch):
-            instruction,value = batch[idx].split(' ')
+        for idx,line in enumerate(batch):
 
-            if idx in processedInst:
-                print(f'This is a repeated instruction. Current accumulator value is {accumulator}')
-                break
-            else:
-                processedInst.append(idx)
+            modifiedBatch = batch.copy()
+            instruction,value = line.split(' ')
 
-            if instruction == 'acc':
-                accumulator += int(value)
-                idx += 1
+            if instruction == 'nop':
+                modifiedBatch[idx] = line.replace('nop', 'jmp')
             elif instruction == 'jmp':
-                idx += int(value)
-            else:
-                idx += 1
+                modifiedBatch[idx] = line.replace('jmp', 'nop')
+
+            result,accumulator = runBatch(modifiedBatch)
+
+            if result:
+                print(f'Succesful program termination with accumulator value {accumulator}')
+                break
+
 
     except IOError:
         print(f'Unable to open file {inputPath}')
