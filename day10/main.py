e8628eb5a5526efeaa4083120d0fd76138cb95eb
Aled Parry <aled.skyrail@gmail.com>
2020-12-10 17:01:47 +0000

    Day 10 - part 2 - finally succesful. Been pulling my hair out over this one - created a graph of what was linked to what and then recursed through it. Needed help from Jonathan Paulson on Reddit whose video cleared up why my recursive function was taking forever!

--- day10/main.py
+++ day10/main.py
@@ -1,4 +1,34 @@
 import os, sys
+from functools import reduce
+
+def buildGraph(values):
+    graph = dict()
+
+    for value in values:
+        for difference in range(1,4):
+            if value + difference in values:
+                if value in graph:
+                    graph[value].append(value + difference)
+                else:
+                    graph[value] = [value + difference]
+    
+    return graph
+
+def countPathways(graph, start, end, calculated={}):
+    pathways = 0
+
+    if start in calculated:
+        return calculated[start]
+
+    if start == end:
+        pathways += 1
+    else:
+        nodes = graph[start]
+        for node in nodes:
+            pathways += countPathways(graph, node, end)
+    
+    calculated[start] = pathways
+    return pathways
 
 if len(sys.argv) != 2:
     raise ValueError('Please supply the input to be processed')
@@ -16,12 +46,17 @@ if os.path.isfile(inputPath):
         batch.insert(0, 0)
         batch.append(max(batch)+3)
 
+        # Part one
         while index < (len(batch) - 1):                
             differences.append(batch[index+1] - batch[index])
             index += 1
         
-
         print(f'The differences that count: {differences.count(1)*differences.count(3)}')
+
+        # Part two
+        graph = buildGraph(batch)
+
+        print(f'Number of pathways: {countPathways(graph, 0, max(batch))}')
 
     except IOError:
         print(f'Unable to open file {inputPath}')
