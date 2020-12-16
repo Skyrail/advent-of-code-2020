2dd25d9f7fe4bad5a153ae0243e3ae9b5d938bf3
Aled Parry <aled.skyrail@gmail.com>
2020-12-16 15:38:31 +0000

    Day 16 - part 2 - succes - albeit I feel like it's some of the worst code I've ever written. It could do with a thorough refactor and some major efficiency improvements but right now I've other stuff to be doing...

--- day16/main.py
+++ day16/main.py
@@ -1,4 +1,6 @@
 import os, sys, re
+from functools import reduce
+from collections import defaultdict
 
 if len(sys.argv) != 2:
     raise ValueError('Please supply the input to be processed')
@@ -8,20 +10,22 @@ inputPath = sys.argv[1]
 def parseRules(ruleText):
     rules = dict()
     lines = ruleText.split('\n')
-    ruleRegex = r'([a-z\s]+): ([0-9-]+) or ([0-9-]+)'
+    ruleRegex = r'([a-z\s]+): ([0-9]+)-([0-9]+) or ([0-9]+)-([0-9]+)'
 
     for rule in lines:
         result = re.match(ruleRegex, rule)
         field = result.group(1)
-        rules[field] = [r.split('-') for r in result.group(2,3)]
+        minX,maxX,minY,maxY = [int(x) for x in result.group(2,3,4,5)]
+        rules[field] = set(range(minX,maxX+1)) | set(range(minY,maxY+1))
     
     return rules
 
 def isValid(value, rules):
-    for limits in rules.values():
-        for limit in limits:
-            if int(limit[0]) <= int(value) <= int(limit[1]):
-                return True
+    
+    allRules = reduce(lambda x,y: x.union(y), rules.values())
+
+    if value in allRules:
+        return True
 
     return False
 
@@ -31,25 +35,45 @@ if os.path.isfile(inputPath):
         
         blocks = file.read().strip().split('\n\n')
 
-        # Input segments:
-        # - everything up until 'your ticket:' is the validation
-        # - from 'your ticket:' to 'nearby tickets:' is your ticket numbers
-        # - from 'nearby tickets:' is the other tickets
-
         rules,myTicket,otherTickets = blocks
 
         rules = parseRules(rules)
         myTicket = myTicket.split('\n')[1].split(',')
         otherTickets = [ticket.split(',') for ticket in otherTickets.split('\n')[1:]]
 
-        invalidValues = list()
+        validTickets = list()
 
         for ticket in otherTickets:
+            isValidTicket = True
             for value in ticket:
-                if isValid(value, rules) is False:
-                    invalidValues.append(int(value))
+                if isValid(int(value), rules) is False:
+                    isValidTicket = False
+            if isValidTicket:
+                validTickets.append(ticket)
 
-        print(f'Ticket scanning error rate is: {sum(invalidValues)}')
+        validFields = defaultdict(set)
+
+        for field,rule in rules.items():
+            for i in range(len(validTickets[0])):
+                if all(int(ticket[i]) in rules[field] for ticket in validTickets):
+                    validFields[field].add(i)
+
+        while all([len(L) == 1 for L in validFields.values()]) == False:
+            for field,indicies in validFields.items():
+                if len(indicies) == 1:
+                    for f,i in validFields.items():
+                        if field == f:
+                            continue
+                        validFields[f] = i - indicies
+
+        result = 1
+
+        for field,index in validFields.items():
+            if 'departure' in field:
+                result *= int(myTicket[list(index)[0]])
+
+        print(f'The result is {result}')
+        
 
     except IOError:
         print(f'Unable to open file {inputPath}')
